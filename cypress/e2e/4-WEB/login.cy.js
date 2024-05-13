68bebc63b048d33fe80f59b5f81614af55d900cb
elvanisa <elvanisamuhsina@gmail.com>
2024-05-13 11:32:58 +0700

    Update Login Scenario

--- cypress/e2e/4-WEB/login.cy.js
+++ cypress/e2e/4-WEB/login.cy.js
@@ -1,12 +1,20 @@
 describe('Test Saucedemo', () => {
-  it('berhasil login', () => {
+  it.only('berhasil login', () => {
     // id menggunakan #
     // class menggunakan .
     // sisanya menggunakan []
-    
     cy.visit('https://www.saucedemo.com/') //open page
-    cy.get('#user-name').type('standard_') //input username
+    cy.get('#user-name').type('standard_user') //input username
     cy.get('[data-test="password"]').type('secret_sauce') //input password
     cy.get('.submit-button.btn_action').click() //klik tombol login
+    cy.get('.app_logo').should('be.visible') //verifikasi berhasil login
+    cy.url().should('include', 'inventory.html')
+  })
+  it('gagal login', () => {
+    cy.visit('https://www.saucedemo.com/')
+    cy.get('[data-test="username"]').type('locked_out_user')
+    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('[data-test="login-button"]').click()
+    cy.get('[data-test="error"]').should('contain.text', 'this user has been locked out')
   })
 })
